de7144f9a24682d3112f113f4631e610f084587f
RuanDevz <Ruanbatista1509@outlook.com>
2024-10-12 21:37:08 -0300

    get plan

--- models/user.js
+++ models/user.js
@@ -34,7 +34,26 @@ module.exports = (sequelize, DataTypes) => {
         vipExpirationDate: { // Campo para armazenar a data de expiração do VIP
             type: DataTypes.DATE,
             allowNull: true, // Permite que o campo seja nulo inicialmente
-        }
+        },
+        lastLogin: { // Campo para armazenar a última vez que o usuário fez login
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        recentlyViewed: { // Lista de conteúdos visualizados recentemente
+            type: DataTypes.ARRAY(DataTypes.STRING), // Armazena um array de strings
+            allowNull: true,
+            defaultValue: [], // Inicializa com um array vazio
+        },
+        transactions: { // Lista de transações feitas pelo usuário
+            type: DataTypes.JSONB, // Usando JSONB para armazenar um array de objetos de transações
+            allowNull: true,
+            defaultValue: [], // Inicializa com um array vazio
+        },
+        favorites: { // Lista de conteúdos favoritos do usuário
+            type: DataTypes.JSONB, // Usando JSONB para armazenar um array de objetos de favoritos
+            allowNull: true,
+            defaultValue: [], // Inicializa com um array vazio
+        },
     }, {});
 
     return User;
